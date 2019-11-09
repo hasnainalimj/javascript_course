d07cf0497d985e8743c9d1ddd21b99a2cdcfeb74
hasnainalimj <mjhasnain15@gmail.com>
2019-11-09 22:10:15 +0500

    Class one

---
+++ Class 1/app.js
@@ -0,0 +1,225 @@
+// alert("Thanks for your input!");
+
+// window.alert("Hello Worlds");
+
+var name = "Javascript";
+// console.log("name", name);
+
+var _pi = 3.142 + 2200;
+// console.log("TCL: _pi", _pi);
+
+var city;
+
+city = "Karachi";
+
+// alert(city);
+
+var flag = false;
+// console.log("TCL: flag", flag);
+var flag2 = true;
+// console.log("TCL: flag2", flag2);
+
+// var firstNameUsernameEmail //camel case appraoch
+
+var n1 = 5;
+var n2 = 5;
+
+// alert(n1 + n2);
+// alert(n1 - n2);
+// alert(n1 * n2);
+// alert(n1 / n2);
+// alert(n1 % n2);
+
+var whatsLeftOver = 10 % 3;
+// console.log("TCL: whatsLeftOver", whatsLeftOver);
+
+var n3 = 5;
+
+//1 pre increment //1
+//2 post increment //1
+//3 pre decrement //1
+//4 post decrement //1
+// ++n3;
+// console.log(++n3);
+
+// console.log(n3++);
+
+// console.log(n3);
+// --n3;
+// console.log(--n3);
+
+// n3--;
+// console.log(n3);
+
+var num1 = 10;
+//10 +    11    +    11        -   12
+// console.log(num1 + ++num1 + num1++ - num1);
+//9     +   8      +  8     -   10
+// console.log(--num1 + --num1 + num1++ - ++num1);
+
+// var calc = 1 + 5 * 6;  //31
+// var calc = (1 + 5) * 6;
+var calc = (1 + 5) * (6 / 6);
+// console.log("TCL: calc", calc);
+
+var resultOfComputation = 2 * 4 * 4 + 2;
+// console.log("TCL: resultOfComputation", resultOfComputation);
+
+var name = "Faizan";
+
+var city = "Karachi";
+
+// alert(name + " " + city);
+
+// var name = prompt("Enter your name");
+// console.log("TCL: name", name);
+
+// var age = +prompt("Enter your age");
+// console.log("TCL: age", age);
+
+// var email = prompt("Enter your email", "mjhasnain15@gmail.com");
+// console.log("TCL: email", email);
+
+//Comparision Operators
+//1 <
+//2 >
+//3 <=
+//4 >=
+// ===
+// ==
+// !==
+
+// var num1 = +prompt("enter n1");
+// var num2 = +prompt("enter n2");
+
+//<
+// if (num1 < num2) {
+//   console.log("num1 small hy");
+// }
+
+//>
+// if (num1 > num2) {
+//   console.log("num1 greater hy");
+// }
+
+//<=
+// if (num1 <= num2) {
+//   console.log("num1 is equal or small hy");
+// }
+
+//>=
+// if (num1 >= num2) {
+//   console.log("num1 is equal or greater hy");
+// }
+
+//==
+// if (num1 == num2) {
+//   console.log("both are equal");
+// }
+
+// !==
+// if (num1 !== num2) {
+//   console.log("unequal");
+// }
+
+var str = "123";
+var nums = 123;
+var flg = true;
+var flg2 = "true";
+
+//===
+// if (str === nums) {
+//   console.log("both matched");
+// } else {
+//   console.log("not matched");
+// }
+
+// if (flg === flg2) {
+//   console.log("both matched");
+// } else {
+//   console.log("not matched");
+// }
+
+// var age = +prompt("Enter your age");
+
+// if(age == 18){
+//     console.log("allowed")
+// }else{
+//     console.log("not allowed");
+// }
+
+//Else If
+// if (age == 18) {
+//   console.log("allowed");
+// } else if (age == 25) {
+//   console.log("age is 25");
+// } else if (age == 200) {
+//   console.log("what");
+// } else {
+//   console.log("invalid input");
+// }
+
+// var country = "Pakistans";
+
+// if (country == "Pakistan") {
+//   var area = "Kalaboard";
+//   if (area == "Malir") {
+//     console.log("malir");
+//   } else if (area == "ShahFaisal") {
+//     console.log("shah faisal");
+//   } else {
+//     console.log("invalid input");
+//   }
+// } else {
+//   var area = "the";
+//   if (area == "Abc") {
+//     console.log("Abc");
+//   } else if (area == "xyz") {
+//     console.log("xyz");
+//   } else {
+//     console.log("invalid input");
+//   }
+// }
+
+//Conditional Operators
+//1 &&
+//2 ||
+
+//&&
+var int1 = 5;
+var int2 = 6;
+
+// if (int1 == 5 && int2 == 6) {
+//   console.log("alls good");
+// }
+
+// if (int1 == 6 || int2 == 6) {
+//   console.log("alls good 2");
+// }
+
+// if ((int1 == 5 && int1 == 4) || int2 == 5 || int2 == 6) {
+//   console.log("perfect");
+// }
+
+//Non Premitive
+//Array
+
+var data = [1, 2, 3, 4, 5, 6, "Abc", "Xyz", true, false, []];
+
+// console.log("length of array", data.length);
+// console.log(data[7]);
+
+// data[10] = "Array Tha Yaha";
+
+// data[100] = "100 Values";
+
+// console.log("TCL: data", data);
+
+var cities = [
+  "Atlanta",
+  "Baltimore",
+  "Chicago",
+  "Denver",
+  "Los Angeles",
+  "Seattle"
+];
