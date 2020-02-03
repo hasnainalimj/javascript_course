ade6af07be49e7f204de60da98c39fda7facdca2
hasnainalimj <mjhasnain15@gmail.com>
2020-02-02 17:05:59 -0800

    js class 11

---
+++ Class 11/app.js
@@ -0,0 +1,30 @@
+localStorage.setItem("string","faizan")
+// localStorage.setItem("number",123123)
+// localStorage.setItem("boolean",false)
+
+// var string = localStorage.getItem("string");
+// var number = localStorage.getItem("number");
+// var boolean = localStorage.getItem("boolean");
+
+// console.log("TCL: string", string)
+// console.log("TCL: number", number)
+// console.log("TCL: boolean", boolean)
+
+// localStorage.removeItem("string")
+// localStorage.removeItem("number")
+// localStorage.removeItem("boolean")
+
+// var marks = [250 , 240 , 300]
+// var obj = {
+//     name : "Faizan",
+//     field : "Development"
+// }
+
+// localStorage.setItem("marks" , JSON.stringify(marks))
+// localStorage.setItem("student" , JSON.stringify(obj))
+
+// var arr = JSON.parse(localStorage.getItem("marks"))
+// var obj2 = JSON.parse(localStorage.getItem("student"))
+
+// console.log("TCL: arr", arr)
+// console.log("TCL: obj2", obj2)
